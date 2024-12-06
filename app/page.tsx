ffd17f355be92b810707aebf8d877020e2c01e68
Abdallah El Madi <abdael-m@c4r6p5.1337.ma>
2024-12-06 15:49:05 +0100

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home(): React.JSX.Element {
   return (
     <main className="text-[28px] text-[--global-color]">
-      End old late about!
+      End old late about!?
     </main>
   )
 }
