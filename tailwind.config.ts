cba8d6de7bbc60b6324a7ef343f11fab7319a96e
abdallahelmadi <abdallahelmadi42@gmail.com>
2024-12-08 09:02:01 +0100

    day-1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,18 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    screens: {
+      'md': '849px',
+      'lg': '1106px',
+      'xl': '1362px',
+      '2xl': '1617px'
     },
+    extend: {}
   },
-  plugins: [],
-} satisfies Config;
+  plugins: []
+} satisfies Config
